3eb2918f03ba2c8868ba316ceaf38058fb7c42a1
Eric Muntas <112870448+EricMuntas@users.noreply.github.com>
2023-06-13 18:17:07 +0200

    Update firebase.js

--- CodeClicker/app/firebase.js
+++ CodeClicker/app/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP1FpqERTFVSoFGifw3pqyOh4Z-KYchEc",
-  authDomain: "m12-login.firebaseapp.com",
-  projectId: "m12-login",
-  storageBucket: "m12-login.appspot.com",
-  messagingSenderId: "78032116279",
-  appId: "1:78032116279:web:6a1b8784b962405d127db2",
-  measurementId: "G-YZP8575L72",
+//
 };
 
 // Initialize Firebase
